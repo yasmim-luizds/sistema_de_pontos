b5173346c10cbf356ed15d158188ab537e79033a
Yasmim Luiz <yasmimluiz1989@gmail.com>
2025-10-18 20:42:08 -0300

    correção no create table

--- src/create_tables_and_records.py
+++ src/create_tables_and_records.py
@@ -1,54 +1,57 @@
 from conexion.oracle_queries import OracleQueries
 
-def create_tables(query:str):
+def create_tables(query: str):
     list_of_commands = query.split(";")
 
     oracle = OracleQueries(can_write=True)
     oracle.connect()
 
-    for command in list_of_commands:    
-        if len(command) > 0:
+    for command in list_of_commands:
+        if len(command.strip()) > 0:
             print(command)
             try:
                 oracle.executeDDL(command)
                 print("Successfully executed")
             except Exception as e:
-                print(e)            
+                print(f"Error executing command: {e}")
 
-def generate_records(query:str, sep:str=';'):
+def generate_records(query: str, sep: str = ';'):
     list_of_commands = query.split(sep)
 
     oracle = OracleQueries(can_write=True)
     oracle.connect()
 
-    for command in list_of_commands:    
-        if len(command) > 0:
+    for command in list_of_commands:
+        if len(command.strip()) > 0:
             print(command)
-            oracle.write(command)
-            print("Successfully executed")
+            try:
+                oracle.write(command)
+                print("Successfully executed")
+            except Exception as e:
+                print(f"Error inserting record: {e}")
 
 def run():
-
-    with open("../sql/create_tables_pedidos.sql") as f:
+    # scripts certos do projeto de pontos
+    with open("../sql/create_tables.sql") as f:
         query_create = f.read()
 
     print("Creating tables...")
     create_tables(query=query_create)
     print("Tables successfully created!")
 
-    with open("../sql/inserting_samples_records.sql") as f:
-        query_generate_records = f.read()
+    with open("../sql/insert_records_func.sql") as f:
+        query_generate_func = f.read()
 
-    print("Gerenating records")
-    generate_records(query=query_generate_records)
-    print("Records successfully generated!")
+    print("Generating FUNCIONARIOS records...")
+    generate_records(query=query_generate_func)
+    print("FUNCIONARIOS records successfully generated!")
 
-    with open("../sql/inserting_samples_related_records.sql") as f:
-        query_generate_related_records = f.read()
+    with open("../sql/insert_records_marc.sql") as f:
+        query_generate_marc = f.read()
 
-    print("Gerenating records")
-    generate_records(query=query_generate_related_records, sep='--')
-    print("Records successfully generated!")
+    print("Generating MARCACOES records...")
+    generate_records(query=query_generate_marc)
+    print("MARCACOES records successfully generated!")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
